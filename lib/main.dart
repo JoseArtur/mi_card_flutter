d1c247bcce4a96a1bb1f6de885b342f695bbe33a
José Artur <65734671+JoseArtur@users.noreply.github.com>
2020-06-30 16:46:31 -0300

    Learnig more About widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+  MyApp()
   );
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+         child:Container(  // A container only allows one widget
+           height: 100.0,
+           width: 100.0,
+           padding: EdgeInsets.all(20), //Padding is the inside of the widget
+           margin:EdgeInsets.only(left:30), //Margin is the outside of the widget
+           color: Colors.white,
+           child:Text('Hello'
+         )
+        ),
+      ),
+      ),
+    );
+  }
 }
