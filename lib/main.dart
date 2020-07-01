7001ed79adcb77c19ec7d39bada5119bc3d771a7
José Artur <65734671+JoseArtur@users.noreply.github.com>
2020-07-01 19:33:06 -0300

    My Card Update

--- lib/main.dart
+++ lib/main.dart
@@ -9,43 +9,76 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.teal[500],
         body: SafeArea(
-            child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              // A container only allows one widget
-
-              height: 100.0,
-              width: 100.0,
-              color: Colors.red,
-            ),
-            SizedBox(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                   Container(
-                    color:Colors.yellow,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    color:Colors.green,
-                    width: 100,
-                    height: 100,
-                  ),
-                ],
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/myimage.png.png'),
+                backgroundColor: Colors.white12,
               ),
-            ),
-            Container(
-              // A container only allows one widget
-              width: 100.0,
-              color: Colors.blue,
-            ),
-          ],
-        )),
+              Text(
+                'José Artur',
+                style: TextStyle(
+                    fontFamily: 'Pacifico', fontSize: 40, color: Colors.white),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      size: 30,
+                      color: Colors.teal.shade700,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+55 98 99995-0506',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'SourceSansPro',
+                          color: Colors.teal.shade500,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.email, size: 30, color: Colors.teal.shade700),
+                    SizedBox(width: 10,),
+                    Text(
+                      'joseartura1.2@gmail.com',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal.shade500),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
